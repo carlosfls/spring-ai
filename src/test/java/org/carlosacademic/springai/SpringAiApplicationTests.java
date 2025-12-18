962247408502e8799aa02069ff906e310bdfe9e0
Carlos <carlosfls54@gmail.com>
2025-12-17 23:55:49 -0300

    Initial commit

---
+++ src/test/java/org/carlosacademic/springai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosacademic.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

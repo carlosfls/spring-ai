325677f7f7338c94bb6c05b618f59ba8c038eabb
Carlos <carlosfls54@gmail.com>
2025-12-25 23:26:36 -0300

    Load prompt template from resource file and update `askQuestion` to use it instead of hardcoded text.

--- src/main/java/org/carlosacademic/springai/service/OpenIABoardGameService.java
+++ src/main/java/org/carlosacademic/springai/service/OpenIABoardGameService.java
@@ -3,6 +3,8 @@ package org.carlosacademic.springai.service;
 import org.carlosacademic.springai.model.Answer;
 import org.carlosacademic.springai.model.Question;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service("open-ai-board-game-service")
@@ -10,20 +12,23 @@ public class OpenIABoardGameService implements BoardGameService{
 
     private final ChatClient chatClient;
 
+    /**
+     * Using a resource to load the prompt template from the classpath.
+     * The resource contains the question and the parameters to be replaced.
+     * This way is better than having the prompt hardcoded in the code.
+     */
+    @Value("classpath:/prompts/questionPromptTemplate.st")
+    Resource questionPromptTemplate;
+
     public OpenIABoardGameService(ChatClient.Builder chatClient) {
         this.chatClient = chatClient.build();
     }
 
     @Override
     public Answer askQuestion(Question question) {
-        String text = """
-                Answer the question about:
-                The title of the question is {title}.
-                The question is: {question}.
-                """;
         var answer = chatClient.prompt()
                 .user(promptUserSpec ->
-                        promptUserSpec.text(text)
+                        promptUserSpec.text(questionPromptTemplate)
                                 .param("title", question.title())
                                 .param("question", question.question())
                 )

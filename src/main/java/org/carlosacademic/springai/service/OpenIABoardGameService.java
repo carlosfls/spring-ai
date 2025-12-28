9c15944275b91b30d68f745b018ef6143da8e9bb
Carlos <carlosfls54@gmail.com>
2025-12-28 12:59:18 -0300

    Setting the temperature option for more deterministic results.

--- src/main/java/org/carlosacademic/springai/service/OpenIABoardGameService.java
+++ src/main/java/org/carlosacademic/springai/service/OpenIABoardGameService.java
@@ -3,6 +3,7 @@ package org.carlosacademic.springai.service;
 import org.carlosacademic.springai.model.Answer;
 import org.carlosacademic.springai.model.Question;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -27,13 +28,18 @@ public class OpenIABoardGameService implements BoardGameService{
     }
 
     /**
-     * The prompt accepts a String or a Resource.
-     * Using the system option to send the instructions to the LLM.
-     * And the user option to send the question to the LLM.
+     * Adding the options for the chat prompt.
+     * Temperature is one option set to make the chat more deterministic or more random.
+     * The values are between 0 and 2 with much higher value meaning more randomness.
      */
     @Override
     public Answer askQuestion(Question question) {
         var rules = rulesService.getRulesFor(question.title());
+
+        var chatOptions = ChatOptions.builder()
+                .temperature(0.7)
+                .build();
+
         var answer = chatClient.prompt()
                 .system(promptSystemSpec ->
                         promptSystemSpec.text(promptTemplate)
@@ -41,6 +47,7 @@ public class OpenIABoardGameService implements BoardGameService{
                                 .param("rules", rules)
                 )
                 .user(question.question())
+                .options(chatOptions)
                 .call()
                 .content();
 
